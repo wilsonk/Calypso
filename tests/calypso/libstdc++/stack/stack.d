abcd9e04b353ead6cc707ba27b7a82e17054253f
Kelly Wilson <wilsonk@cpsc.ucalgary.ca>
2015-04-16 23:04:57 -0600

    Update stack and list examples

--- tests/calypso/libstdc++/stack/stack.d
+++ tests/calypso/libstdc++/stack/stack.d
@@ -17,10 +17,22 @@ import (C++) std.deque;
 void main()
 {
     auto dq = new deque!(int);
+    for (int i = 0;i<10; i++)
+      dq.push_back(i);
+
     //auto v = new vector!(int)(2,200);   //FAILURE
 
     auto st1 = new stack!char;
     auto dq2 = new stack!(int)(dq);
+    //auto st2 = new stack!(int, vector!int);    //FAILURE
 
     writeln("stack appears to work");
+    writeln("empty stack size = ", st1.size());
+    writeln("10 element deque stack size = ", dq2.size());
+
+    for (int i = 0;i<10; i++)
+    {
+        writeln("popping the top of a stack of 10 elements from ordered deque = ", dq2.top());
+        dq2.pop();
+    }
 }
