dd827b58c923b645edf761bb43e352568710a9fc
Elie Morisse <syniurge@gmail.com>
2015-05-10 14:01:47 -0300

    Updated Qt5 demo this time with AddressBook inheriting from QWidget.

--- tests/calypso/qt5/qt5demo.d
+++ tests/calypso/qt5/qt5demo.d
@@ -1,76 +1,61 @@
 /**
  * D adaptation of the Qt5 Widgets first tutorial.
  *
- * Build with:
- *   $ ldc2 -g -wi -v -cpp-args -D_REENTRANT -cpp-args -fPIE -cpp-args -DQT_WIDGETS_LIB -cpp-args -DQT_GUI_LIB -cpp-args -DQT_CORE_LIB -cpp-args -I/path/to/Qt/5.4/gcc_64/mkspecs/linux-g++ -cpp-args -I/path/to/Qt/5.4/gcc_64/include -cpp-args -I/path/to/Qt/5.4/gcc_64/include/QtWidgets -cpp-args -I/path/to/Qt/5.4/gcc_64/include/QtGui -cpp-args -I/path/to/Qt/5.4/gcc_64/include/QtCore -L-rpath=/path/to/Qt/5.4/gcc_64 -L-rpath=/path/to/Qt/5.4/gcc_64/lib -L-lQt5Widgets -L-lQt5Gui -L-lQt5Core -L-lGL -L"-lstdc++" qt5demo.d
+ * Module map files from utils/modulemap/ should be installed in the libc and Qt include folders.
+ *
+ * Then build with:
+ *   $ ldc2 -g -wi -v -cpp-args -D_REENTRANT -cpp-args -fPIE -cpp-args -DQT_WIDGETS_LIB -cpp-args -DQT_GUI_LIB -cpp-args -DQT_CORE_LIB -cpp-args -I/path/to/Qt/5.4/gcc_64/mkspecs/linux-g++ -cpp-args -I/path/to/Qt/5.4/gcc_64/include -cpp-args -I/path/to/Qt/5.4/gcc_64/include/QtWidgets -cpp-args -I/path/to/Qt/5.4/gcc_64/include/QtGui -cpp-args -I/path/to/Qt/5.4/gcc_64/include/QtCore -L-rpath=/path/to/Qt/5.4/gcc_64 -L-rpath=/path/to/Qt/5.4/gcc_64/lib -L-lQt5Widgets -L-lQt5Gui -L-lQt5Core -L-lGL -L-lstdc++ qt5demo.d
  */
 
 modmap (C++) "<QtWidgets>";
 
 import core.runtime;
 import std.stdio, std.conv;
-import (C++) Qt.AlignmentFlag, QApplication, QString;
+
+import (C++) Qt.QtCore;
+import (C++) Qt.AlignmentFlag;
+
+import (C++) QApplication, QString;
 import (C++) QWidget, QTextEdit, QLineEdit, QLabel, QLayout, QGridLayout;
 
-// class AddressBook : QWidget
-// {
-// public:
-//     this(QWidget *parent = null)
-//     {
-//         super(parent);
-//
-//         auto nameStr = new QString("Name:");
-//         auto addressStr = new QString("Address:");
-//         auto titleStr = new QString("Simple Address Book");
-//
-//         auto nameLabel = new QLabel(nameStr);
-//         nameLine = new QLineEdit;
-//
-//         auto addressLabel = new QLabel(addressStr);
-//         addressText = new QTextEdit;
-//
-//         auto mainLayout = new QGridLayout;
-//         mainLayout.addWidget(cast(QWidget*) nameLabel, 0, 0); // explicit casts are needed, why?
-//         mainLayout.addWidget(cast(QWidget*) nameLine, 0, 1);
-//         mainLayout.addWidget(cast(QWidget*) addressLabel, 1, 0/+, QFlags!AlignmentFlag(AlignmentFlag.AlignTop)+/);
-//         mainLayout.addWidget(cast(QWidget*) addressText, 1, 1);
-//
-//         setLayout(cast(QLayout*) mainLayout);
-//         setWindowTitle(titleStr);
-//     }
-//
-// private:
-//     QLineEdit nameLine;
-//     QTextEdit addressText;
-// }
+class AddressBook : QWidget
+{
+public:
+    this(QWidget *parent = null)
+    {
+        super(parent);
+
+        auto nameStr = new QString("Name:");
+        auto addressStr = new QString("Address:");
+        auto titleStr = new QString("Simple Address Book");
+
+        auto nameLabel = new QLabel(nameStr);
+        nameLine = new QLineEdit;
+
+        auto addressLabel = new QLabel(addressStr);
+        addressText = new QTextEdit;
+
+        auto mainLayout = new QGridLayout;
+        mainLayout.addWidget(cast(QWidget*) nameLabel, 0, 0); // explicit casts are needed, why?
+        mainLayout.addWidget(cast(QWidget*) nameLine, 0, 1);
+        mainLayout.addWidget(cast(QWidget*) addressLabel, 1, 0, new Alignment(AlignmentFlag.AlignTop));
+        mainLayout.addWidget(cast(QWidget*) addressText, 1, 1);
+
+        setLayout(cast(QLayout*) mainLayout);
+        setWindowTitle(titleStr);
+    }
+
+private:
+    QLineEdit nameLine;
+    QTextEdit addressText;
+}
 
 int main()
 {
-    auto app = new QApplication(Runtime.cArgs.argc, Runtime.cArgs.argv);
+    auto app = new QApplication(Runtime.cArgs.argc,
+                Runtime.cArgs.argv);
 
-//     auto addressBook = new AddressBook;
-//     addressBook.show();
-
-    auto addressBook = new QWidget;
-
-    auto nameStr = new QString("Name:");
-    auto addressStr = new QString("Address:");
-    auto titleStr = new QString("Simple Address Book");
-
-    auto nameLabel = new QLabel(nameStr);
-    auto nameLine = new QLineEdit;
-
-    auto addressLabel = new QLabel(addressStr);
-    auto addressText = new QTextEdit;
-
-    auto mainLayout = new QGridLayout;
-    mainLayout.addWidget(cast(QWidget*) nameLabel, 0, 0); // explicit casts are needed, why?
-    mainLayout.addWidget(cast(QWidget*) nameLine, 0, 1);
-    mainLayout.addWidget(cast(QWidget*) addressLabel, 1, 0/+, QFlags!AlignmentFlag(AlignmentFlag.AlignTop)+/);
-    mainLayout.addWidget(cast(QWidget*) addressText, 1, 1);
-
-    addressBook.setLayout(cast(QLayout*) mainLayout);
-    addressBook.setWindowTitle(titleStr);
+    auto addressBook = new AddressBook;
     addressBook.show();
 
     return app.exec();
